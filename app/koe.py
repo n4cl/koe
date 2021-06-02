7c5f69285663dca7cc07449dead1fbdeeb49bc36
n4cl <devn4cl@gmail.com>
2021-06-02 23:48:08 +0900

    Update: ページを辿れるように更新

--- app/koe.py
+++ app/koe.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import configparser
 import requests
 from bs4 import BeautifulSoup
@@ -16,6 +17,7 @@ class Koe:
 
     def fetch_user(self, _key, _page) -> list:
         ref = []
+        is_next_page = False
 
         res = requests.get(self._url + "/search.php?word=%s&g=1&m=1&p=%s" % (_key, _page))
         bs = BeautifulSoup(res.text, "html.parser")
@@ -24,9 +26,16 @@ class Koe:
             _href = _a["href"]
             if _href[:10] == "detail.php":
                 ref.append(_a["href"])
+            elif _a.string == "Next":
+                is_next_page = True
+
+        if is_next_page:
+            time.sleep(2)
+            self.fetch_user(_key, _page+1)
+
         return ref
 
 
 if __name__ == "__main__":
     koe = Koe()
-    print(koe.fetch_user("あ", 1))
+    print(koe.fetch_user("a", 1))

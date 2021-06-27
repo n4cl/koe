3210691c7abdb6ebb1f246a7bbedfdeb3f575191
n4cl <devn4cl@gmail.com>
2021-06-28 01:08:45 +0900

    Add: ストレージを追加

--- app/create_db.py
+++ app/create_db.py
@@ -1,11 +1,12 @@
 import sqlite3
-con = sqlite3.connect('koe.db', isolation_level=None)
+con = sqlite3.connect('/usr/local/storage/koe.db', isolation_level=None)
 
 con.execute("BEGIN")
 
 con.execute("""
 CREATE TABLE content(
     id INTEGER PRIMARY KEY,
+    user TEXT,
     title TEXT,
     body TEXT,
     file TEXT,

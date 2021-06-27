a65449c9eeb4babfab98696da3aaaef86c8d4997
LiptonLover <devn4cl@gmail.com>
2021-06-28 02:09:31 +0900

    データ格納先を追加 (#6)
    * Add: ストレージを追加
    
    * Fix: カラムの順を修正

--- app/create_db.py
+++ app/create_db.py
@@ -1,17 +1,18 @@
 import sqlite3
-con = sqlite3.connect('koe.db', isolation_level=None)
+con = sqlite3.connect('/usr/local/storage/koe.db', isolation_level=None)
 
 con.execute("BEGIN")
 
 con.execute("""
 CREATE TABLE content(
     id INTEGER PRIMARY KEY,
+    user TEXT,
     title TEXT,
     body TEXT,
     file TEXT,
     play_time INTEGER,
+    view INTEGER,
     fav INTEGER,
-    views INTEGER,
     created_date TEXT,
     updated_date TEXT
 )""")
